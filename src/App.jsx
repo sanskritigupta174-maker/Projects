4ae5cf9edc4f4cfe54eee9a9dbbab9ddcaf4c3a7
Sanskriti Gupta <sanskritigupta174@gmail.com>
2026-03-12 22:52:53 +0530

    Implemented Lazy loading for performance optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
-import { Stars } from '@react-three/drei';
+import {lazy, Suspense} from 'react';
+import { Hero, Navbar} from './components';
+
+const About = lazy(() =>
+  import("./components").then((module) => ({ default: module.About })));
+const Contact = lazy(() => 
+  import("./components").then((module) => ({default: module.Contact})));
+const Experience = lazy(() => 
+  import("./components").then((module) => ({default: module.Experience})));
+const Tech = lazy(() => 
+  import("./components").then((module) => ({default: module.Tech})));
+const Works = lazy(() => 
+  import("./components").then((module) => ({default: module.Works})));
+const StarsCanvas = lazy(() => 
+  import("./components/canvas").then((module) => ({default: module.StarsCanvas})));
 
 function App() 
 {
   return (
     <BrowserRouter>
+    <Suspense>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
        <Navbar/>
@@ -15,12 +29,13 @@ function App()
         <Experience/>
         <Tech/>
         <Works/>
-        <Feedbacks/>
+        {/* <Feedbacks/> */}
         <div className='relative z-0'>
           <Contact/>
           <StarsCanvas/>
         </div>
       </div>
+      </Suspense>
       </BrowserRouter>
   )
 }
