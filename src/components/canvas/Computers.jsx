561e55f9a3a82278a43aecceb5758850aae27115
Sanskriti Gupta <sanskritigupta174@gmail.com>
2026-03-14 00:42:31 +0530

    Fix: CSS fixes for camvas model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -19,7 +19,7 @@ const Computers = ({ isMobile}) => {
       />
       <primitive object={computer.scene}
       scale={isMobile ? 0.7 : 0.75}
-      position={isMobile ? [0, -3, -2.2] :[0, -3.25, -1.5]}
+      position={isMobile ? [0, -2.8, -2.2] : [1.2, -3.2, -1.5]}
       rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -51,8 +51,8 @@ const ComputersCanvas = () =>
   }, []);
 
   return (
-    <Canvas frameloop='demand' shadows camera={{position: [20, 3, 5], fov:25}} gl={{preserveDrawingBuffer : true}}>
-    <Suspense fallback={<CanvasLoader/>}>
+   <Canvas frameloop='demand' shadows camera={{position: [20, 3, 5], fov:25}} gl={{preserveDrawingBuffer : true}}>
+   <Suspense fallback={<CanvasLoader/>}>
     <OrbitControls 
     enableZoom={false}
     maxPolarAngle={Math.PI/2}
@@ -60,7 +60,7 @@ const ComputersCanvas = () =>
     />
     <Computers isMobile={isMobile}/>
     </Suspense>
-    <Preload all/>
+    <Preload />
     </Canvas>
   )
 }
